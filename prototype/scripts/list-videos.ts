67f12cd53af3182f9849d5fe8b859b402787aa87
Gian Tristian G. Apostol <89404207+SpectralQuill@users.noreply.github.com>
2026-03-06 00:16:49 +0800

    Initiated Vite+React-TS.

--- prototype/scripts/list-videos.ts
+++ prototype/scripts/list-videos.ts
@@ -1,14 +1,14 @@
 #!/usr/bin/env node
 import path from "path";
-import { getVideoList } from "../src/get-video-list";
+import Video from "../src/video";
 
 async function main() {
     const targetDir = process.argv[2];
     if (!targetDir) {
-        console.error("Error: A folder path argument is required.\nUsage: tsx scripts/list-videos.ts <folder>");
+        console.error("Error: A folder path argument is required.\nUsage: npm run list-videos.ts <folder>");
         process.exit(1);
     }
-    const videos = await getVideoList(targetDir);
+    const videos = await Video.getVideoList(targetDir);
     // Print only file names
     for (const fullPath of videos) {
         console.log(path.basename(fullPath));
