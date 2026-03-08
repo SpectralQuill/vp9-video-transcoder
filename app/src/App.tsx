d6975d29574fad71bef7e00074d227a52a7f9203
Gian Tristian G. Apostol <89404207+SpectralQuill@users.noreply.github.com>
2026-03-08 17:46:46 +0800

    Improved title bar.

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,6 +1,8 @@
 import { Prototype } from "./components/Prototype";
 import { TitleBar } from "./components/TitleBar";
 
+import "./style/App.scss";
+
 export default function App() {
 
     return <>
--- app/src/components/TitleBar.tsx
+++ app/src/components/TitleBar.tsx
@@ -1,32 +1,60 @@
+import {
+    faMinus,
+    faXmark
+} from '@fortawesome/free-solid-svg-icons';
+import {
+    faWindowMaximize,
+    faWindowRestore
+} from '@fortawesome/free-regular-svg-icons';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+    useEffect,
+    useRef,
+    useState
+} from "react";
 import { WebviewWindow } from "@tauri-apps/api/webviewWindow";
 
 import "../style/TitleBar.scss";
 
 export function TitleBar() {
-
-    const appWindow = new WebviewWindow("main");
     
-    const minimize = () => appWindow.minimize();
-    const maximize = async () => {
-        const maximized = await appWindow.isMaximized();
-        maximized ? appWindow.unmaximize() : appWindow.maximize();
+    const appWindow = useRef(new WebviewWindow("main")).current;
+    const [maximized, setMaximized] = useState<boolean>(true);
+    
+    const minimize = async () => appWindow.minimize();
+    const toggleMaximize = async () => {
+        await (maximized ? appWindow.unmaximize() : appWindow.maximize());
+        setMaximized(!maximized);
     };
-    const close = () => appWindow.close();
+    const close = async () => appWindow.close();
+
+    useEffect(() => {(async () => {
+
+        const maximized = await appWindow.isMaximized();
+        setMaximized(maximized);
+
+    })()}, []);
     
     return <>
-        <div
-            id="title-bar"
-        >
-            <div style={{ display: "flex", alignItems: "center" }}>
+        <div className="title-bar" data-tauri-drag-region>
+            <div className="title-wrapper" data-tauri-drag-region>
                 {/* <img src="/logo.png" alt="Logo" style={{ height: 20, marginRight: 8 }} /> */}
-                <span>VP9 Video Transcoder</span>
+                <span data-tauri-drag-region>VP9 Video Transcoder</span>
                 {/* <button style={{ marginLeft: 12, WebkitAppRegion: "no-drag" }}>← Back</button>
                 <button style={{ WebkitAppRegion: "no-drag" }}>→ Forward</button> */}
             </div>
             <div className="window-control-buttons-wrapper">
-                <button onClick={minimize}>─</button>
-                <button onClick={maximize}>⬜</button>
-                <button onClick={close}>✕</button>
+                <button onClick={minimize}>
+                    <FontAwesomeIcon icon={faMinus} />
+                </button>
+                <button onClick={toggleMaximize}>
+                    <FontAwesomeIcon
+                        icon={maximized? faWindowRestore : faWindowMaximize}
+                    />
+                </button>
+                <button onClick={close}>
+                    <FontAwesomeIcon icon={faXmark} />
+                </button>
             </div>
         </div>
     </>;
